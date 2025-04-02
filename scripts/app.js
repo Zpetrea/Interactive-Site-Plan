40549052fc9e7a3c3136ee4620319749c572094a
Zachary Petrea <93063718+Zpetrea@users.noreply.github.com>
2025-04-02 11:05:26 -0400

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -8,7 +8,7 @@ const closePopup = document.querySelector('.popup-close');
 const sitePlan = document.getElementById('site-plan');
 
 // List of doors that need the exception styling (Update this list based on the site plan)
-const exceptionDoors = [5, 6, 7, 16, 31, 32, 33, 34, 35, 36, 45, 46]; // Replace with actual exception door numbers
+const exceptionDoors = [5, 6, 7, 15, 16, 21, 23, 24, 25, 26, 28, 29, 30, 31, 32, 33, 34, 35, 36, 43, 44, 45, 46]; // Replace with actual exception door numbers
 
 document.addEventListener("DOMContentLoaded", () => {
     // Highlight exception doors by adding a CSS class and setting tooltips
